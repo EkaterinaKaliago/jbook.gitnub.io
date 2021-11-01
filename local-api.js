4e8e82496bf5deee288f72e7d28c84857af7f72e
kate <kaliago@yandex.ru>
2021-11-01 12:34:44 +0300

    add lerna

---
+++ local-api.js
@@ -0,0 +1,11 @@
+// import express
+
+
+// one rout
+// serve up react production assets
+
+// save a list of cells into a file
+
+// load up a list of cells from a file
+
+// express server listen om port 4050
